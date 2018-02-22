2cb5de41705b72792b74e3a4c065dc09a3730fcd
Alok Jha <alok4588@gmail.com>
2018-02-22 15:40:26 +0530

    Initial Commit

---
+++ Discovery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Discovery
+//
+//  Created by Alok Jha on 22/02/18.
+//  Copyright © 2018 Alok Jha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
